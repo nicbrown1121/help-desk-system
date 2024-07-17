5149c078b5cd4b3b8cc2e8b427a98788c24fa88a
Nicole Brown <nicbrown@udel.edu>
2024-07-17 13:38:35 -0400

    added tailwindCSS to homepage

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -2,13 +2,19 @@ import { Link } from 'react-router-dom';
 
 export const Home = () => {
   return (
-    <div>
-      <h1>Welcome to the Ticketing System</h1>
-      <ul>
-          <Link to="/submit-ticket">Submit a Ticket</Link>
-        </ul>
-        <ul>
-          <Link to="/view-tickets">View Tickets</Link>
+    <div className="flex flex-col justify-center items-center pt-20 text-black dark:text-white">
+      <h1 className="text-4xl font-bold mb-8">Welcome to the Ticketing System</h1>
+      <ul className="space-y-4">
+        <li>
+          <Link to="/submit-ticket" className="text-blue-500 hover:underline text-lg font-bold dark:text-blue-300">
+            Submit a Ticket
+          </Link>
+        </li>
+        <li>
+          <Link to="/view-tickets" className="text-blue-500 hover:underline text-lg font-bold dark:text-blue-300">
+            View Tickets
+          </Link>
+        </li>
       </ul>
     </div>
   );
