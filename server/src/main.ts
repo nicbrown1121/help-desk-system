978f5f84a41b38155722b6cb7b7f161fae2b3409
Nicole Brown <nicbrown@udel.edu>
2024-07-17 13:38:19 -0400

    remove commented out code

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const configService = app.get(ConfigService);
-  // const originUrl = process.env.ORIGIN_URL;
-  // console.log({ originUrl });
+
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
